9ffd758bf65d5a768a49beb2a6b4ee9050fa82db
Anna Winther <anski.anna@gmail.com>
2020-03-19 13:48:58 +0000

    Starting to psuedo code out the problem

--- adv.py
+++ adv.py
@@ -24,16 +24,40 @@ world.load_graph(room_graph)
 world.print_rooms()
 
 player = Player(world.starting_room)
-
+# print('PLAYEERRRR!!', player.current_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
 # TASK: CONSTRUCT MY OWN TRAVERSAL GRAPH
 # Start in room 0 which contains exits ['n', 's', 'w', 'e'].
-# DONE TRAVERSING when there are exactly 500 entries in the graph and no '?' in the adjacency dictionaries
+# DONE TRAVERSING when there are exactly 500 entries in the graph and no '?' in the adjacency dictionaries - BASE CASE?
 # This traversal algorithm logs the path into traversal_path. 
-#  
+
+# I need to create a loop that:
+    # Tried to move in a direction
+    # If in a new room, fill in the entries in the graph, showing that the previous room has an exit that lead to the current room an vice versa.
+
+    # Do this until all rooms have been visited and there are 500 entries in the graph with no '?' in the dictionary.
+
+# Do a DFS for everything past '?' unitl it reaches a room with no unexplored paths (dead-end).
+    # Add the rooms to the traversal path while going through them.
+    # When hitting a dead-end, move back to the nearest room that contains an unexplored path. Until room with exit '?'.
+
+# Use BFS to find the shortest path to an unexplored room. (Use code from earlier this week, but searching for the '?'). This will find the nearest room with '?' for exit
+    # If an exit has been exlored, add it in BFS queue
+    # Make sure to convert the path returned to a list of n/s/e/w directions before adding it to traversal path.
+
+
+# CREATAE SEPARATE FUNCTIONS FOR BFS AND DFS
+
+# 
+
+
+
+
+
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
