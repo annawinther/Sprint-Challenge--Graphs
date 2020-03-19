cefce669c16a0ffaa9e37a302a23b194b3038350
Anna Winther <anski.anna@gmail.com>
2020-03-19 12:52:02 +0000

    Initial commit

--- adv.py
+++ adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
+map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,7 +29,11 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-
+# TASK: CONSTRUCT MY OWN TRAVERSAL GRAPH
+# Start in room 0 which contains exits ['n', 's', 'w', 'e'].
+# DONE TRAVERSING when there are exactly 500 entries in the graph and no '?' in the adjacency dictionaries
+# This traversal algorithm logs the path into traversal_path. 
+#  
 
 # TRAVERSAL TEST
 visited_rooms = set()
